60709e254ee1ca534a81ef0a385e4c1373f84e2c
Prince kumar Singh <princekumar2000.pks@gmail.com>
2021-12-07 23:31:29 +0530

    v2

--- server.php
+++ server.php
@@ -1,26 +1,26 @@
-<?php
+   <?php
 
-$conn =mysqli_connect("localhost","root","","dev_bugger");
+   $conn =mysqli_connect("localhost","root","","dev_bugger");
 
-if(isset($_POST['submit'])){
+   if(isset($_POST['submit'])){
 
-   $name=$_POST['name'];
-   $phone=$_POST['phone'];
-   $email=$_POST['email'];
-   $gender=$_POST['gender'];
-   $password=md5($_POST['password']);
-   $dup_pass=$_POST['password'];
-   $current_year=$_POST['current_year'];
-   $branch = $_POST['branch'];
-   $field=$_POST['field'];
+      $name=$_POST['name'];
+      $phone=$_POST['phone'];
+      $email=$_POST['email'];
+      $gender=$_POST['gender'];
+      $password=md5($_POST['password']);
+      $dup_pass=$_POST['password'];
+      $current_year=$_POST['current_year'];
+      $branch = $_POST['branch'];
+      $field=$_POST['field'];
 
-   $sql = "INSERT INTO user(name,email,gender,mob,password,dup_password,year,branch,field_instereseted_in)VALUES('{$name}','{$email}','{$gender}','{$phone}','{$password}','{$dup_pass}','{$current_year}','{$branch}','{$field}')";
-   $result=mysqli_query($conn,$sql);
-   if($result)
-   {
-      echo "Success";
+      $sql = "INSERT INTO user(name,email,gender,mob,password,dup_password,year,branch,field_instereseted_in)VALUES('{$name}','{$email}','{$gender}','{$phone}','{$password}','{$dup_pass}','{$current_year}','{$branch}','{$field}')";
+      $result=mysqli_query($conn,$sql);
+      if($result)
+      {
+         echo "Success";
+      }
+      else{
+         echo "Failed";
+      }
    }
-   else{
-      echo "Failed";
-   }
-}
